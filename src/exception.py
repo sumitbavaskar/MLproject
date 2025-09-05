acc7464944a08091d3500b3917c3e0292456d9c5
sumitbavaskar <sumitbawaskar68@gmail.com>
2025-09-05 15:15:30 +0530

    EDA and Problem Statement

---
+++ src/exception.py
@@ -0,0 +1,23 @@
+import sys
+from logger import logging
+
+def error_massage_details(error,error_details:sys):
+    _,_,exc_tb=error_details.exc_info()
+    filename=exc_tb.tb_frame.f_code.co_filename
+    error_massage="Error occured in python script name[{0}] line number[{1}] error massage[{2}]".format(
+        filename,exc_tb.tb_lineno,str(error)
+       
+    )
+    return error_massage
+
+class CustomException(Exception):
+    def __init__(self,error_massage,error_details:sys):
+        super().__init__(error_massage)
+        self.error_massage=error_massage_details(error_massage,error_details=error_details)
+
+    def __str__(self):
+        return self.error_massage    
+
+
+
+
